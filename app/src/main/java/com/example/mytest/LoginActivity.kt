da91572610b2f641ef8cf32f5eb543434485fbf0
hyemmmm <105183531+hyemmmm@users.noreply.github.com>
2022-05-14 14:52:09 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mytest/LoginActivity.kt
@@ -0,0 +1,19 @@
+package com.example.mytest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        setupEvents()
+        setValues()
+    }
+    fun setupEvents(){
+
+    }
+    fun setValues(){
+
+    }
+}
